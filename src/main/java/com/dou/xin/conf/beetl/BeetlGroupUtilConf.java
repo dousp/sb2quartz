6a919c677c11132497d9bc586bf76c80c0ecbde0
douspeng <douspeng@sina.cn>
2019-05-05 15:28:04 +0800

    删除注释

--- src/main/java/com/dou/xin/conf/beetl/BeetlGroupUtilConf.java
+++ src/main/java/com/dou/xin/conf/beetl/BeetlGroupUtilConf.java
@@ -1,23 +1,5 @@
-/**
- * Copyright 2018-2020 stylefeng & fengshuonan (https://gitee.com/stylefeng)
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.dou.xin.conf.beetl;
 
-// import cn.stylefeng.guns.core.tag.DictSelectorTag;
-// import cn.stylefeng.guns.core.util.KaptchaUtil;
-// import cn.stylefeng.roses.core.util.ToolUtil;
 import com.dou.xin.core.utils.ObjectUtil;
 import org.beetl.core.Context;
 import org.beetl.core.Function;
